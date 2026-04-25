4734940b0398c049103646a58aa247305b4b072d
Abdelrahman <abdoalfarsy267@gmail.com>
2026-04-24 17:27:31 -0700

    adding variables

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
 print("hello world")
+"""
+x=5
+if x>3:
+    print("the greatest")
+else:
+    print("hbd")
+    """
+x=9
+s=20
+result=((10*2)+50/5//23*(20%10))
+print(result)
+id=int(5)
+print(20+id)
