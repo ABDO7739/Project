45a0d029cae40e4310d81981945e0585151e7c29
Abdelrahman <abdoalfarsy267@gmail.com>
2026-04-30 13:27:40 -0700

    changing graph

--- index.py
+++ index.py
@@ -1,25 +1,20 @@
-
-graph = {
-    "Porta Nuova":                [("KFC Milano Duomo", 5)],
-    "KFC Milano Duomo":           [("Porta Nuova", 5), ("Milano Centrale Station", 3), ("Navigli", 4)],
-    "Navigli":                    [("KFC Milano Duomo", 4)],
-    "Milano Centrale Station":    [("KFC Milano Duomo", 3), ("Torino Porta Susa", 140), ("Bologna Centrale", 215)],
-    "Torino Porta Susa":          [("Milano Centrale Station", 140), ("Verona Porta Nuova", 280)],
-    "Verona Porta Nuova":         [("Torino Porta Susa", 280), ("Bologna Centrale", 145)],
-    "Bologna Centrale":           [("Milano Centrale Station", 215), ("Verona Porta Nuova", 145), ("Roma Termini Station", 375), ("Firenze Santa Maria Novella", 110)],
-    "Roma Termini Station":       [("Bologna Centrale", 375), ("McDonald's Roma Termini", 2), ("Napoli Centrale Station", 445)],
-    "McDonald's Roma Termini":    [("Roma Termini Station", 2), ("Trastevere", 8), ("EUR District", 5)],
-    "Trastevere":                 [("McDonald's Roma Termini", 8)],
-    "EUR District":               [("McDonald's Roma Termini", 5)],
-    "Firenze Santa Maria Novella":[("Bologna Centrale", 110), ("Subway Firenze Centro", 44)],
-    "Subway Firenze Centro":      [("Firenze Santa Maria Novella", 44), ("Oltrarno", 31), ("Pisa Centrale", 25), ("Campo di Marte", 112)],
-    "Oltrarno":                   [("Subway Firenze Centro", 31)],
-    "Pisa Centrale":              [("Subway Firenze Centro", 25), ("Burger King Napoli Centrale", 78)],
-    "Campo di Marte":             [("Subway Firenze Centro", 112)],
-    "Burger King Napoli Centrale":[("Pisa Centrale", 78), ("Napoli Centrale Station", 56), ("Vomero", 15)],
-    "Napoli Centrale Station":    [("Roma Termini Station", 445), ("Burger King Napoli Centrale", 56)],
-    "Vomero":                     [("Burger King Napoli Centrale", 15), ("Posillipo", 19)],
-    "Posillipo":                  [("Vomero", 19)]
+campus_graph = {
+    "Entrance": {"Administration": 4, "Dorms": 5},
+    "Administration": {"Entrance": 4, "Library": 6, "Student center": 5, "Lecture 1 Hall": 2},
+    "Library": {"Administration": 6, "Lecture 3 Hall": 3, "Student center": 2, "Lab 1": 4},
+    "Lecture 3 Hall": {"Library": 3, "Lab 1": 2, "Student Affairs Office": 5, "Electronics Lab": 9},
+    "Student center": {"Administration": 5, "Library": 2, "Lab 1": 1, "Lecture 1 Hall": 4, "Parking": 8},
+    "Lab 1": {"Student center": 1, "Library": 4, "Lecture 3 Hall": 2, "Student Affairs Office": 7},
+    "Lecture 1 Hall": {"Administration": 2, "Student center": 4, "Dorms": 3, "Parking": 4},
+    "Dorms": {"Entrance": 5, "Lecture 1 Hall": 3, "Parking": 7, "Lecture 2 Hall": 3},
+    "Parking": {"Lecture 1 Hall": 4, "Dorms": 7, "Lecture 2 Hall": 9, "Gym": 11, "Student center": 8},
+    "Lecture 2 Hall": {"Dorms": 3, "Parking": 9, "Lab 2": 5},
+    "Gym": {"Parking": 11, "Lab 2": 8, "Student Affairs Office": 6},
+    "Lab 2": {"Lecture 2 Hall": 5, "Gym": 8, "Physics laboratory": 12},
+    "Student Affairs Office": {"Lab 1": 7, "Lecture 3 Hall": 5, "Gym": 6, "Physics laboratory": 4, "Electronics Lab": 8},
+    "Physics laboratory": {"Student Affairs Office": 4, "Lab 2": 12, "IT": 10},
+    "Electronics Lab": {"Lecture 3 Hall": 9, "Student Affairs Office": 8, "IT": 11},
+    "IT": {"Physics laboratory": 10, "Electronics Lab": 11},
 }
 def bfs(start , goal):
     visited = []
@@ -35,7 +30,8 @@ def bfs(start , goal):
                fronter .append((city,curr_path+[city]))
     return "No solution found!"
 
-sol = bfs("Milano Centrale Station" , "Posillipo")
+sol = bfs("Entrance" ,"IT")
+#BFS algorithm
 def uniform_cost2(start,goal):
     visited = []
     fronter = [(0,start,[start])]
@@ -53,6 +49,6 @@ def uniform_cost2(start,goal):
                     curr_path + [curr_city]
                 ))
                 return"No Solution found!"
-            sol,cost = uniform_cost2("Milano Centrale Station","Posillipo")
+            sol,cost = uniform_cost2("Entrance","IT")
             print(f"{sol}\nTotal cost = {cost}")
 
